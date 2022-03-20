596358d936e0c4c4a8a7baf34a05587fd80831e3
kazi ashiqur rahman <rahman.kaziashiqur.info@gmail.com>
2022-03-20 11:31:25 +0000

    new commit for token

--- src/main/java/com/mycompany/mavenproject1/ashiq.java
+++ src/main/java/com/mycompany/mavenproject1/ashiq.java
@@ -11,5 +11,6 @@ package com.mycompany.mavenproject1;
 public class ashiq {
     public static void main(String[] args) {
         System.out.println("ashiq");
+        System.out.println("sdfjnskdnfbksnbdf");
     }
 }
